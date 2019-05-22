0ce18dd0ab7ebb6d0e9cde324b9fccab71e7b5b6
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 22:32:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,6 +18,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "select projects.title, sum(pledges.amount)
    from projects 
    join pledges on projects.id = pledges.project_id
+   where sum(pledges.amount) > 100
    group by pledges.project_id"
 end
 
