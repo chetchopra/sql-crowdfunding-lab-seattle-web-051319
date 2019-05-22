ff14341b9363ce808a1728b9ab0683d47cc7c903
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 22:46:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,9 +26,9 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "select users.name, sum(pledges.amount) as col
    from users
-   join pledges on user.id = pledges.user_id
+   join pledges on users.id = pledges.user_id
    group by users.name
-   order by col, user.name"
+   order by col, users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
