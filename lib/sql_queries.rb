27c1402a28035ef6c1ed50128077fc25416b6b69
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 22:30:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,8 +18,9 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "select projects.title, sum(pledges.amount) as collected
    from projects 
    join pledges on projects.id = pledges.project_id
+   group by pledges.project_id
    where collected >= projects.funding_goal
-   group by pledges.project_id"
+   "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
