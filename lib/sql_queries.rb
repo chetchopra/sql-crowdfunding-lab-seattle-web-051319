3a8522dd26dda68113945a5c30cbf98bf9563279
Chet  Chopra <chopra.chet@gmail.com>
2019-05-22 22:39:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "select projects.title, sum(pledges.amount) as col
+  "select projects.title, sum(pledges.amount) - projects.funding_goal as col
    from projects 
    join pledges on projects.id = pledges.project_id
    group by pledges.project_id
